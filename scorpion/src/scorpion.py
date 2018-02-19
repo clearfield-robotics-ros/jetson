1a496d907e5727dffaae0e4c736bfa5e6e641868
David Robinson <dvsrobinson@gmail.com>
2018-02-19 13:13:28 -0500

    added origin tf

--- scorpion/src/scorpion.py
+++ scorpion/src/scorpion.py
@@ -8,75 +8,75 @@ from visualization_msgs.msg import Marker
 import tf
 import math
 
-# in: command of sweeping / position
-# out: position of gantry (geometry_msgs/Point)
+model_offset_rot = [math.pi/2, 0, math.pi]
+model_offset_loc = [4.25,-4.25,0]
 
-# todo: second thread for restart
+def updateLocation(loc, rot):
 
-seq = 0
+	# send transform
+    q_rot = tf.transformations.quaternion_from_euler(rot[0],rot[1],rot[2])
+    br.sendTransform(loc,
+         q_rot,
+         rospy.Time.now(),
+         "scorpion_tf",
+         "world")
 
+    # send model to rviz
+    m = Marker()
+    m.header.frame_id = "world"
+    m.header.seq = 0
+    m.header.stamp = rospy.Time.now()
+    m.ns = "scorpion"
+    m.id = 0
+    m.type = 10  # mesh
+    m.mesh_resource = "package://scorpion/mesh/scorpion_master.STL";
+    m.action = 0  # add/modify
 
-def updateLocation(data):
+    scopion_rot = [rot[0] + model_offset_rot[0],
+    	rot[1] + model_offset_rot[1],
+    	rot[2] + model_offset_rot[2]]
 
-    global seq
+    scopion_q_rot = tf.transformations.quaternion_from_euler(scopion_rot[0],scopion_rot[1],scopion_rot[2])
+    m.pose.position.x = loc[0] + model_offset_loc[0]
+    m.pose.position.y = loc[1] + model_offset_loc[1]
+    m.pose.position.z = loc[2] + model_offset_loc[2]
+    m.pose.orientation.x = scopion_q_rot[0]
+    m.pose.orientation.y = scopion_q_rot[1]
+    m.pose.orientation.z = scopion_q_rot[2]
+    m.pose.orientation.w = scopion_q_rot[3]
 
-    # # make point for other ROS nodes
-    # msg = PointStamped()
-    # msg.point = Point(a, b, float(data.data))
-    # msg2.header.frame_id = "world"
-    # msg2.header.seq = seq
-    # msg2.header.stamp = rospy.Time.now()
-    # pub.publish(msg)
-
-    # make marker for viz
-    msg2 = Marker()
-    msg2.header.frame_id = "world"
-    msg2.header.seq = seq
-    msg2.header.stamp = rospy.Time.now()
-    msg2.ns = "scorpion"
-    msg2.id = seq
-    msg2.type = 10  # mesh
-    msg2.mesh_resource = "package://scorpion/src/scorpion_master.STL";
-    msg2.action = 0  # add/modify
-
-    msg2.pose.position = data
-    msg2.pose.orientation.x = 1.0
-    msg2.pose.orientation.y = 0.0
-    msg2.pose.orientation.z = 0.0
-    msg2.pose.orientation.w = 1.0
-
-    msg2.scale.x = 0.01
-    msg2.scale.y = 0.01
-    msg2.scale.z = 0.01
-    msg2.color.a = 1.0
-    msg2.color.r = 0.5
-    msg2.color.g = 0.5
-    msg2.color.b = 0.5
-    pub.publish(msg2)
-
-    # seq += 1
+    m.scale.x = 0.01
+    m.scale.y = 0.01
+    m.scale.z = 0.01
+    m.color.a = 1.0
+    m.color.r = 0.5
+    m.color.g = 0.5
+    m.color.b = 0.5
+    pub.publish(m)
 
 
 def main():
 
     global pub
+    global br
+    br = tf.TransformBroadcaster()
 
     rospy.init_node('scorpion')
     pub = rospy.Publisher('scorpion', Marker, queue_size=10)
     rospy.sleep(1)
 
-    r = rospy.Rate(1)  # 10 Hz
+    r = rospy.Rate(10)  # 10 Hz
 
+    # generate manually, but eventually get from localization!
 
-    pose = Point()
-    pose.x = 0
-    pose.y = 0
-    pose.z = 0
+    loc = [0,0,0]
+    rot = [0,0,0]
+
 
     while not rospy.is_shutdown():
 
-    	pose.x += 0.1
-    	updateLocation(pose)
+    	loc[1] += 0.05 # advance!
+    	updateLocation(loc, rot)
 
         r.sleep()  # indent less when going back to regular gantry_lib
 
