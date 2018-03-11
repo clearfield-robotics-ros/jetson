8ea9b54802063c88ccb07c6d4700ca70a8a3d0ab
David Robinson <dvsrobinson@gmail.com>
2018-03-11 18:43:41 -0400

    fixed 2nd state planner

--- probe/src/probe_planner.py
+++ probe/src/probe_planner.py
@@ -21,20 +21,10 @@ jetson_current_state = rospy.Subscriber('current_state', Int16, update_state)
 
 def probe_to_gantry_transform(loc,yaw):
 
-    # print "yaw (rad)", yaw
-
     Hprobe = np.array([[1,0,0,loc.x],
                        [0,1,0,loc.y],
                        [0,0,1,loc.z],
                        [0,0,0,1]])
-
-    # H = Hprobe
-    # trans = np.matmul(H,np.array([[0],[0],[0],[1]]))
-    # br.sendTransform((trans[0],trans[1],trans[2]),
-    #    tf.transformations.quaternion_from_euler(0,0,0),
-    #    rospy.Time.now(),
-    #    "H1",
-    #    "gantry")
 
     Hyaw = np.array([[math.cos(yaw),-math.sin(yaw),0,0],
                      [math.sin(yaw),math.cos(yaw),0,0],
@@ -51,15 +41,6 @@ def probe_to_gantry_transform(loc,yaw):
                    [0,0,1,0],
                    [0,0,0,1]])
 
-    # H = Hprobe.dot(Hyaw).dot(Hyrot).dot(Hd)
-    # trans = np.matmul(H,np.array([[0],[0],[0],[1]]))
-    #
-    # br.sendTransform((trans[0],trans[1],trans[2]),
-    #    tf.transformations.quaternion_from_euler(0,0,0),
-    #    rospy.Time.now(),
-    #    "H2",
-    #    "gantry")
-
     Hoffset = np.array([[1,0,0,-probe_base_offset_loc[0]],
                        [0,1,0,-probe_base_offset_loc[1]],
                        [0,0,1,-probe_base_offset_loc[2]],
@@ -68,18 +49,12 @@ def probe_to_gantry_transform(loc,yaw):
     H = Hprobe.dot(Hyaw).dot(Hoffset).dot(Hyrot).dot(Hd)
     trans = np.matmul(H,np.array([[0],[0],[0],[1]]))
 
-    # br.sendTransform((trans[0],trans[1],trans[2]),
-    #    tf.transformations.quaternion_from_euler(0,0,0),
-    #    rospy.Time.now(),
-    #    "H3",
-    #    "gantry")
-
     return trans
 
 def move_gantry(desired_probe_tip, gantry_yaw):
 
     print "MOVE GANTRY TO:", desired_probe_tip
-    plot_point(desired_probe_tip.x,desired_probe_tip.y,desired_probe_tip.z)
+    plot_point(desired_probe_tip.x,desired_probe_tip.y,desired_probe_tip.z,[0,1,0],30)
 
     # work out gantry carriage position
     trans = probe_to_gantry_transform(desired_probe_tip, gantry_yaw)
@@ -115,7 +90,7 @@ def update_probe_state(data):
     probe_current_state = data.data
 
 contact_viz_id = 0
-def plot_point(x,y,z):
+def plot_point(x,y,z,col,size):
     # Visualize probe point
     global contact_viz_id
     global contact_viz_pub
@@ -129,13 +104,13 @@ def plot_point(x,y,z):
     msg.action = 0  # add
     msg.pose.position = Point(x,y,z)
     msg.pose.orientation.w = 1
-    msg.scale.x = 30
-    msg.scale.y = 30
-    msg.scale.z = 30
+    msg.scale.x = size
+    msg.scale.y = size
+    msg.scale.z = size
     msg.color.a = 0.25
-    msg.color.r = 0.0
-    msg.color.g = 1.0
-    msg.color.b = 0.0
+    msg.color.r = col[0]
+    msg.color.g = col[1]
+    msg.color.b = col[2]
     contact_viz_pub.publish(msg)
     contact_viz_id += 1
 
@@ -167,7 +142,8 @@ def main():
     probe_angle = rospy.get_param('probe_base_offset_rot')[1]
     scorpion_gantry_offset_loc = rospy.get_param('scorpion_gantry_offset_loc')
     global probe_length
-    probe_length = (scorpion_gantry_offset_loc[2] + probe_base_offset_loc[2] + abs(landmine_pos[2])) / math.sin(probe_angle)
+    probe_length = (scorpion_gantry_offset_loc[2] + probe_base_offset_loc[2]
+        + abs(landmine_pos[2])) / math.sin(probe_angle)
     maxForwardSearch = math.cos(probe_angle)*landmine_height*(1/probe_safety_factor);
     gantry_width = rospy.get_param('gantry_width')
 
@@ -201,8 +177,15 @@ def main():
     global est
     est = Mine_Estimator(landmine_diameter, landmine_height)
 
+    # DEBUG
+    # Gantry: 1200,-400,300
+    # landmine_pos: [1800, 100, -150]
 
-    # # DEBUG
+    # target.x = 600
+    # target.y = 500
+    # target.z = -450
+
+    # DEBUG
     # rospy.sleep(1) # Sleeps for 1 sec
     # for i in range(0,10):
     #     x = -landmine_diameter/2*math.sin(180/10*i * math.pi/180)+250
@@ -214,7 +197,7 @@ def main():
     #
     # print(est.get_est())
     # pdb.set_trace()
-    # # DEBUG
+    # DEBUG
 
 
     r = rospy.Rate(100) # Hz
@@ -254,9 +237,14 @@ def main():
 
                     # define desired probe tip position in gantry frame
                     th = np.array([-math.pi/4,0.,math.pi/4])
-                    x = -landmine_diameter/2*np.cos(th) + est.most_recent_point().x
+                    x = landmine_diameter/2*np.cos(th) + est.most_recent_point().x
                     y = landmine_diameter/2*np.sin(th) + est.most_recent_point().y
                     z = np.ones(len(th))*est.most_recent_point().z
+
+                    # DEBUG
+                    # for i in range(0,len(th)):
+                    #     plot_point(x[i],y[i],z[i],[1,0,0],10)
+                    # DEBUG
 
                     if target.y > gantry_width/2:
                         gantry_yaw = th[2] # get desired yaw
@@ -283,6 +271,7 @@ def main():
                     probe_sequence += 1
 
             elif probe_plan_state == 2:
+
                 print "PLAN STATE 2"
                 while True:
                     pass
