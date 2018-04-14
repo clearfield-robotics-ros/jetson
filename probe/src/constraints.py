799756eed868684ea99ae45dfee8abf4b198cb75
Edward Terry <edward.william.terry@gmail.com>
2018-04-14 12:44:47 -0400

    planner works well

--- probe/src/constraints.py
+++ probe/src/constraints.py
@@ -6,24 +6,24 @@ import networkx as nx
 from matplotlib import pyplot as plt
 from shapely.geometry.polygon import LinearRing, Polygon
 
-GANTRY_Y_LIMITS = [0, .800]
+GANTRY_Y_LIMITS = [0, .8]
 GANTRY_TH_LIMITS = [np.deg2rad(-90), np.deg2rad(90)]
 gantry_y_mean = (GANTRY_Y_LIMITS[0] + GANTRY_Y_LIMITS[1])/2
 off_limits = [[(gantry_y_mean+.220, np.deg2rad(-45)), (GANTRY_Y_LIMITS[1], np.deg2rad(-45)), (GANTRY_Y_LIMITS[1], np.deg2rad(30)), (gantry_y_mean+.220, np.deg2rad(30))],
 			  [(gantry_y_mean+.270, np.deg2rad(80)), (GANTRY_Y_LIMITS[1], np.deg2rad(80)), (GANTRY_Y_LIMITS[1], np.deg2rad(90)), (gantry_y_mean+.270, np.deg2rad(90))],
 			  [(gantry_y_mean-.270, np.deg2rad(-90)), (GANTRY_Y_LIMITS[0], np.deg2rad(-90)), (GANTRY_Y_LIMITS[0], np.deg2rad(-75)), (gantry_y_mean-.270, np.deg2rad(-75))]]
-
+fig = plt.figure(1, figsize=(5,5), dpi=90)
+ax = fig.add_subplot(111)
 def check_collision(point):
 	in_collision = [] # create empty array to store collision results
 	for zone in range(len(off_limits)): # go thorugh each off-limits zone # I KNOW THIS IS TERRIBLE PYTHON STYLE >_<
 		poly = Polygon(off_limits[zone]) # create a polygon for that zone
 		in_collision.append(point.within(poly)) # add to the list whether or not it collided
 		# print "here"
-		# x, y = poly.exterior.xy
+		x, y = poly.exterior.xy
 		# fig = plt.figure(1, figsize=(5,5), dpi=90)
 		# ax = fig.add_subplot(111)
-		# ax.plot(x, y, color='#6699cc', alpha=0.7,
-		#     linewidth=3, solid_capstyle='round', zorder=2)
+		ax.plot(x, y, color='#6699cc', alpha=0.7, linewidth=3, solid_capstyle='round', zorder=2)
 		# ax.set_title('Polygon')
 		# plt.show()
 		# raw_input()
@@ -39,8 +39,11 @@ def plan_path(start, end):
 	G = nx.Graph()
 	q_new_index = 0
 	G.add_node(q_new_index)
+	# fig = plt.figure(1, figsize=(5,5), dpi=90)
+	# ax = fig.add_subplot(111)
+	x, y = start.xy
+	ax.scatter(x, y, c='m')
 	while not goal_reached:
-		q_new_index += 1
 		q_rand = sample_random_point()
 		nearest_q_candidate_dist = []
 		for i in range(len(visited_points)):
@@ -48,21 +51,22 @@ def plan_path(start, end):
 		q_near_index = np.argmin(nearest_q_candidate_dist) # choose the point with the lowest distance among those visited
 		q_near = visited_points[q_near_index] # get the actual point (not index)
 		q_new, goal_reached = extend(q_near, q_rand, end, max_dist)
-		# print q_new
-		# check if q_new is in collision
+		x, y = q_new.xy
 		if not check_collision(q_new):
-			fig = plt.figure(1, figsize=(5,5), dpi=90)
-			ax = fig.add_subplot(111)
-			x, y = q_new.xy
-			ax.scatter(x, y)
+			q_new_index += 1
+			ax.scatter(x, y, c='k')
 			plt.pause(0.01)
 			# plt.show(block=True)
 			visited_points.append(q_new)
 			G.add_node(q_new_index)
 			G.add_edge(q_near_index, q_new_index)
 			if goal_reached:
-				print nx.predecessor(G, 0, target=q_new_index)
-				# print nx.shortest_path(G, source=0, target=q_new_index)
+				ax.scatter(x, y, c='g')
+				# print nx.predecessor(G, 0, target=q_new_index)
+				print nx.shortest_path(G, source=0, target=q_new_index)
+		if check_collision(q_new):
+			ax.scatter(x, y, c='r')
+			plt.pause(0.01)
 	plt.show()
 	for i in range(len(visited_points)):
 		print visited_points[i].xy
@@ -96,7 +100,8 @@ def main():
 		print "Goal is out of bounds"
 	else:
 		plan_path(start_point, end_point)
-
+	raw_input()
+	# plt.close(fig)
 	# # test extend - working fine
 	# next_point, done = extend(start_point, Point(600, 0.-1.4), end_point, 0.5)
 	# print next_point, done
